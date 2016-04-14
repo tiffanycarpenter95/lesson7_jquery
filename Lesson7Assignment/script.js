ccee179bea2e2a3ce0e5676bb1a463840559fbc2
tsc13b <tsc13b@my.fsu.edu>
2016-04-14 02:47:36 +0000

    Lesson 7 final commit

---
+++ Lesson7Assignment/script.js
@@ -0,0 +1,19 @@
+/*
+Program Name: Recipie Display Application
+Author: Tiffany Carpenter
+Date: April 12, 2016
+Filename: script.js
+
+*/
+
+//displays the next element after the current target 
+function display(event) {
+    
+    $(event.currentTarget).next().fadeIn("slow");
+    
+    
+}//end of display
+
+//attach event listener to h3 elements to invoke display function when clicked
+
+$("h3").click(display);
